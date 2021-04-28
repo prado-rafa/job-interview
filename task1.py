37eb2bf6fe391db54c11aacc572fa4ebac6563ce
rpradosantos <rpradosantos@apple.com>
2021-04-27 22:46:36 -0300

    feat(task1): Use ord

--- task1.py
+++ task1.py
@@ -1,26 +1,9 @@
 def parseInt(hex):
-    digitValues = {
-        '0': 0,
-        '1': 1,
-        '2': 2,
-        '3': 3,
-        '4': 4,
-        '5': 5,
-        '6': 6,
-        '7': 7,
-        '8': 8,
-        '9': 9,
-        'A': 10,
-        'B': 11,
-        'C': 12,
-        'D': 13,
-        'E': 14,
-        'F': 15
-    }
-
     value = 0
     for i, v in enumerate(hex):
-        value = value * 16 + digitValues.get(v)
+        o = ord(v)
+        toAdd = o - 55 if o >= 65 else o - 48
+        value = value * 16 + toAdd
     
     return value
 
