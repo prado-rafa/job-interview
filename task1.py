fe719f6fb100315112613f0db381b840d879a447
rpradosantos <rpradosantos@apple.com>
2021-04-27 22:52:16 -0300

    feat(task1): Leave more legible

--- task1.py
+++ task1.py
@@ -2,9 +2,15 @@ def parseInt(hex):
     value = 0
     for i, v in enumerate(hex):
         o = ord(v)
-        toAdd = o - 55 if o >= 65 else o - 48
-        value = value * 16 + toAdd
+        if (o >= 65 and o < 75):
+            toAdd = o - 55
+            value = value * 16 + toAdd
+        elif (o >= 48 and o < 58):
+            toAdd = o - 48
+            value = value * 16 + toAdd
+        else:
+            return "Invalid Input"
     
     return value
 
-print(parseInt("1F"))
+print(parseInt("9F9F"))
